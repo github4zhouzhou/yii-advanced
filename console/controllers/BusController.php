d73512586b68f7244b8638a666dc6fa9671a2e90
zhouzhou <v_zhou@outlook.com>
2020-02-13 17:18:02 +0800

    合成图片脚本优化

--- console/controllers/BusController.php
+++ console/controllers/BusController.php
@@ -12,7 +12,6 @@ use Grafika\Color;
 
 class BusController extends Controller
 {
-
 	// /usr/bin/php /workspace/wwwroot/forex/backend/yii qr-image
 	public function actionMergeQrImage($offset = 0, $dir = '')
 	{
@@ -22,7 +21,7 @@ class BusController extends Controller
 		// 合成主图片的路径，以及下载的二维码存储路径
 		$busDir = '/data/cdn/bus';
 
-		$mainImagePath = '/data/cdn/bus/main.jpg';
+		$mainImagePath = '/data/cdn/main.jpg';
 
 		$busInfoList = (new Query())->from('bus_schedules')->offset($offset)->orderBy('id')->all(\Yii::$app->db);
 
@@ -68,7 +67,7 @@ class BusController extends Controller
 					->setData([
 						'plate_number' => $plateNumber,
 						'route_name' => $routeName,
-//						'page' => 'pages/auth/index',
+						'page' => 'pages/auth/index',
 					])
 					->send();
 				$data = $response->getContent();
@@ -124,97 +123,98 @@ class BusController extends Controller
 	}
 
 	// /usr/bin/php /workspace/wwwroot/forex/backend/yii qr-image
-	public function actionQrImage()
-	{
-		$distDir = '/data/cdn/bus-dist';
-		$busDir = '/data/cdn/bus';
-		$imagePath = '/data/cdn/bus/main.jpg';
-		$csvPath = "/data/cdn/bus_schedules.csv";
+//	public function actionQrImage()
+//	{
+//		$distDir = '/data/cdn/bus-dist';
+//		$busDir = '/data/cdn/bus';
+//		$imagePath = '/data/cdn/bus/main.jpg';
+//		$csvPath = "/data/cdn/bus_schedules.csv";
+//
+//		$client = new Client();
+//
+//		$cvs_file = fopen($csvPath,'r'); //开始读取csv文件数据
+//		$i = 0;//记录cvs的行
+//		$stop = 0;
+//		while ($file_data = fgetcsv($cvs_file))
+//		{
+//			$i++;
+//			if($i==1)
+//			{
+//				continue;//过滤表头
+//			}
+//			$stop++;
+//
+//			if($file_data[0]!='')
+//			{
+//				$data[$i] = $file_data;
+//				$id = $file_data[0];
+//				$routeName = $file_data[3];
+//				$plateNumber = $file_data[2];
+//
+//				$baseName = $routeName . '-' . $plateNumber;
+//				$qrName = $id . '-' . 'qr-' . $baseName . '.png';
+//				$mainName = $id . '-' . $baseName . '.jpg';
+//				$md5MainName = $id . '-' . md5($baseName) . '.jpg';
+//				$this->stdout( $stop . ' ' . $routeName . PHP_EOL);
+//
+//				$qrSavePath = $busDir . '/' . $qrName;
+//
+//				$response = $client->createRequest()
+//					->setMethod('POST')
+//					->setFormat(Client::FORMAT_JSON)
+//					->setUrl("https://ncp.zmxyk.com/1/qr")
+//					->setData(['plate_number' => $plateNumber, 'route_name' => $routeName])
+//					->send();
+//
+//				$data = $response->getContent();
+//
+//				if (file_put_contents($qrSavePath, $data)) {
+//				}
+//
+//				//打开主图和子图
+//				$editor = Grafika::createEditor();
+//
+//				$editor->open($mainImg,  $imagePath);
+//				$editor->open($markImg,  $qrSavePath);
+//				$editor->resizeFit($markImg , 680 , 680);
+//
+//
+//				$editor->blend($mainImg, $markImg, 'normal', 1, 'top-left', 260, 335);
+//
+//				$newRounteName = $routeName . ' 路';
+//				$routeLen = strlen($newRounteName);
+//				if ($routeLen > 5) {
+//					$routeStart = 520 - strlen($newRounteName) * 5;
+//				} elseif ($routeLen <= 4) {
+//					$routeStart = 520 - strlen($newRounteName) * 3;
+//				} else {
+//					$routeStart = 520 - strlen($newRounteName) * 4;
+//				}
+//
+//				$newPlateNumber = '车辆信息：' . $plateNumber;
+//				$plateStart = 1580 -  strlen($newPlateNumber) * 4;
+//
+//				$color = new Color("#ffffff");
+//				$color->setAlpha(0.5);
+//				$editor->text($mainImg, $newPlateNumber, 30, $plateStart, 1150, $color, '/usr/share/fonts/msyh.ttf');
+//				$editor->text($mainImg, $newRounteName, 48, $routeStart, 1110, new Color("#3B4257"), '/usr/share/fonts/msyh.ttf');
+//
+//
+//				try {
+//					$mainSavePath = $distDir . '/' . $mainName;
+//					$editor->save($mainImg, $mainSavePath);
+//				} catch (\Exception $exception) {
+//					return '';
+//				}
+//
+////				if ($stop > 20) break;
+//
+//			}
+//
+//		}
+//		fclose($cvs_file);
+//
+//		$this->stdout('OK' . PHP_EOL);
+//	}
 
-		$client = new Client();
-
-		$cvs_file = fopen($csvPath,'r'); //开始读取csv文件数据
-		$i = 0;//记录cvs的行
-		$stop = 0;
-		while ($file_data = fgetcsv($cvs_file))
-		{
-			$i++;
-			if($i==1)
-			{
-				continue;//过滤表头
-			}
-			$stop++;
-
-			if($file_data[0]!='')
-			{
-				$data[$i] = $file_data;
-				$id = $file_data[0];
-				$routeName = $file_data[3];
-				$plateNumber = $file_data[2];
-
-				$baseName = $routeName . '-' . $plateNumber;
-				$qrName = $id . '-' . 'qr-' . $baseName . '.png';
-				$mainName = $id . '-' . $baseName . '.jpg';
-				$md5MainName = $id . '-' . md5($baseName) . '.jpg';
-				$this->stdout( $stop . ' ' . $routeName . PHP_EOL);
-
-				$qrSavePath = $busDir . '/' . $qrName;
-
-				$response = $client->createRequest()
-					->setMethod('POST')
-					->setFormat(Client::FORMAT_JSON)
-					->setUrl("https://ncp.zmxyk.com/1/qr")
-					->setData(['plate_number' => $plateNumber, 'route_name' => $routeName])
-					->send();
-
-				$data = $response->getContent();
-
-				if (file_put_contents($qrSavePath, $data)) {
-				}
-
-				//打开主图和子图
-				$editor = Grafika::createEditor();
-
-				$editor->open($mainImg,  $imagePath);
-				$editor->open($markImg,  $qrSavePath);
-				$editor->resizeFit($markImg , 680 , 680);
-
-
-				$editor->blend($mainImg, $markImg, 'normal', 1, 'top-left', 260, 335);
-
-				$newRounteName = $routeName . ' 路';
-				$routeLen = strlen($newRounteName);
-				if ($routeLen > 5) {
-					$routeStart = 520 - strlen($newRounteName) * 5;
-				} elseif ($routeLen <= 4) {
-					$routeStart = 520 - strlen($newRounteName) * 3;
-				} else {
-					$routeStart = 520 - strlen($newRounteName) * 4;
-				}
-
-				$newPlateNumber = '车辆信息：' . $plateNumber;
-				$plateStart = 1580 -  strlen($newPlateNumber) * 4;
-
-				$color = new Color("#ffffff");
-				$color->setAlpha(0.5);
-				$editor->text($mainImg, $newPlateNumber, 30, $plateStart, 1150, $color, '/usr/share/fonts/msyh.ttf');
-				$editor->text($mainImg, $newRounteName, 48, $routeStart, 1110, new Color("#3B4257"), '/usr/share/fonts/msyh.ttf');
-
-
-				try {
-					$mainSavePath = $distDir . '/' . $mainName;
-					$editor->save($mainImg, $mainSavePath);
-				} catch (\Exception $exception) {
-					return '';
-				}
-
-//				if ($stop > 20) break;
-
-			}
-
-		}
-		fclose($cvs_file);
-
-		$this->stdout('OK' . PHP_EOL);
-	}
 }
