aad5d84ce05b39c9a36cc3ea4128732deb2dce99
zhouzhou <v_zhou@outlook.com>
2020-02-13 13:50:53 +0800

    m

--- console/controllers/BusController.php
+++ console/controllers/BusController.php
@@ -69,7 +69,7 @@ class BusController extends Controller
 				$editor->resizeFit($markImg , 219 , 219);
 
 
-				$start = 130 + strlen($routeName) * 2;
+				$start = 180 - strlen($routeName) * 2;
 				$editor->blend($mainImg, $markImg, 'normal', 1, 'top-left', 90, 117);
 
 				$color = new Color("#ffffff");
